79b174773919e346963fe73270756c6f34603b90
pzajac <test@mail.pl>
2018-11-27 18:33:55 +0100

    CheckNotAndAuthor exercise resolved

--- Automatyzacja/NaiveUnitTest/Class1.cs
+++ Automatyzacja/NaiveUnitTest/Class1.cs
@@ -7,7 +7,7 @@ using Xunit;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 
-namespace GoogleTest
+namespace WebTests
 {
     public class GoogleSearchTest : IDisposable
     {
@@ -49,6 +49,55 @@ namespace GoogleTest
                 }
             }
 
+            Assert.NotNull(expected);
+        }
+    }
+
+    public class MarkowiczProTest : IDisposable
+    {
+        //zmienna/pole w klasie
+        private IWebDriver browser;
+
+        //konstruktor
+        public MarkowiczProTest()
+        {
+            browser = new ChromeDriver();
+        }
+
+        //funkcja zwalniająca obiekt (jeśli jest interfejs IDisposable musi być funkcja Dispose())
+        public void Dispose()
+        {
+            browser.Quit();
+        }
+
+        [Fact]
+        public void CheckNoteAndAuthor()
+        {
+            //Check if correct note is on main page
+            browser.Navigate().GoToUrl("http://markowicz.pro");
+
+            var notes = browser.FindElements(By.CssSelector("header.entry-header"));
+
+            IWebElement expected = null;
+
+            foreach (var result in notes)
+            {
+                var link = result.FindElement(By.CssSelector("h2.entry-title>a"));
+                if (link.GetAttribute("href") == "http://markowicz.pro/o-transformacjach/")
+                {
+                    expected = result;
+
+                    //Open note
+                    link.Click();
+
+                    //Check author
+                    var author_link = browser.FindElement(By.CssSelector("span.author.vcard>a")).GetAttribute("href");
+                    Assert.Equal("http://markowicz.pro/author/rafal-markowicz/", author_link);
+                    break;
+                }
+            }
+
+            //Check if note exists
             Assert.NotNull(expected);
         }
     }
