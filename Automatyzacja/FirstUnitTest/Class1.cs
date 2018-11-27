9652ae6f6cdc415026e05c64ea4b9aa9abe88a53
Krzysztof Baran <krzysztof.baran30@gmail.com>
2018-11-27 16:23:39 +0100

    XunitTestCase

--- Automatyzacja/FirstUnitTest/Class1.cs
+++ Automatyzacja/FirstUnitTest/Class1.cs
@@ -5,14 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace Automatyzacja
+namespace FirstUnitTest
 {
-    class Program
+    public class GoogleSearchTest : IDisposable 
     {
-        static void Main(string[] args)
+        private IWebDriver browser;
+
+        public GoogleSearchTest()
         {
-            IWebDriver browser = new ChromeDriver();
+         browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_Google_out_code_Sprinters()
+        {
+            //throw new Exception();
+
             browser.Navigate().GoToUrl("http://google.com");
 
             var queryBox = browser.FindElement(By.Name("q"));
@@ -21,21 +31,31 @@ namespace Automatyzacja
 
             var serchResults = browser.FindElements(By.CssSelector("div.rc"));
 
+            IWebElement expected = null;
+
             foreach (var result in serchResults)
             {
-              var link =  result.FindElement(By.CssSelector(".r>a"));
+                var link = result.FindElement(By.CssSelector(".r>a"));
                 if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
 
                 {
-                    Console.WriteLine("Znalazłem");
+                    expected = result;
                     break;
                 }
 
             }
 
+            browser.Quit();
+            Assert.NotNull(expected);
+        }
 
-            //browser.Quit();
+        public void Dispose()
+        {
+            if (browser == null)
 
+            {
+                browser.Quit();
+            }
         }
     }
 }
