78c92ba1c13ca953c89f85f62b30e7d2cbd74351
Ela <ekuch@gmail.com>
2018-11-27 16:38:38 +0100

    konstruktor

---
+++ automatyzacja/NaiveUnitTest/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GoogleTests
+{
+    public class GoogleSearchTests: IDisposable
+    {
+        private IWebDriver browser;
+
+        public GoogleSearchTests()
+        {
+            browser = new ChromeDriver(); 
+        }
+
+        [Fact]
+        public void Can_google_out_code_sprinters()
+        {
+            //IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://google.com");
+            var queryBox = browser.FindElement(By.Name("q"));
+
+            //queryBox.SendKeys("code sprinters" + Environment.NewLine);
+            queryBox.SendKeys("code sprinters");
+            queryBox.Submit();
+            var searchResults = browser.FindElements(By.CssSelector("div.rc"));
+
+            IWebElement expected = null;
+
+            foreach (var result in searchResults)
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                {
+                    expected = result;
+                    //Console.WriteLine("Znalazlem");
+                    break;
+                }
+            }
+            
+            Assert.NotNull(expected);
+            
+
+        }
+
+        public void Dispose()
+        {
+            browser.Quit();
+        }
+
+    }
+
+}
