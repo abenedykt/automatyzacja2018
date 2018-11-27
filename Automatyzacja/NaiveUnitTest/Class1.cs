adf8f9ebaaf4da1fa2bef7e1cbf1cd1345cf7bc0
Admin <milena@example.com>
2018-11-27 16:34:08 +0100

    test 1 assercja

--- Automatyzacja/NaiveUnitTest/Class1.cs
+++ Automatyzacja/NaiveUnitTest/Class1.cs
@@ -5,14 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace Automatyzacja
+namespace GoogleTestes
 {
-    class Program
+    public class GoogleSearchTests : IDisposable
     {
-        static void Main(string[] args)
+        private IWebDriver browser;
+
+        public GoogleSearchTests()
         {
-            IWebDriver browser = new ChromeDriver();
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_google_out_cod_sprinters()
+        {
+
             browser.Navigate().GoToUrl("http://google.com");
 
             var queryBox = browser.FindElement(By.Name("q"));
@@ -21,18 +30,25 @@ namespace Automatyzacja
 
             var searchResult = browser.FindElements(By.CssSelector("div.rc"));
 
-            foreach (var result in searchResult) 
+            IWebElement expected = null;
+
+            foreach (var result in searchResult)
 
             {
                 var link = result.FindElement(By.CssSelector(".r>a"));
-                if(link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
                 {
-                    Console.WriteLine("Znalazlem");
+                    expected = result;
                     break;
                 }
             }
 
-            //browser.Quit();
+            Assert.NotNull(expected);
+        }
+
+        public void Dispose()
+        {
+            browser.Quit();
         }
     }
 }
