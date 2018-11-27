af62724b43b445ac03d714d98710106f5e0e031d
Krzysztof Baran <krzysztof.baran30@gmail.com>
2018-11-27 18:25:21 +0100

    Zmiany w KOdzie do poprawy

---
+++ Automatyzacja/ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RafałMarkowiczStrona
+{
+    public class Transformacje : ElementClickInterceptedException
+    {
+        private IWebDriver browser;
+
+        public Transformacje
+        {
+            Browser = new ChromeDriver
+        }
+
+            [Fact]
+
+        public void TransformacIje()
+        {
+            Browser.Navigate().GoToUrl("http://markowicz.pro");
+
+            var querBox = Browser.FindElement(By.LinkText("http://markowicz.pro/o-transformacjach/"));
+            querBox.Click("O Transformacjach");
+            querBox.Submit();
+
+            var serchResults = Browser.FindElements(By.CssSelector("div.rc"));
+
+            foreach (var result in serchResults)
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
+
+                {
+                    Console.WriteLine("Znalazłem");
+                    break;
+                }
+
+            }
+
+
+            //browser.Quit();
+        }
+
+    private class Browser
+    {
+    }
+}
+
+}
---
+++ Automatyzacja/MarkowiczPro/Class1.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarkowiczPro
+{
+    public class GoogleSearchTest : IDisposable
+    {
+        private IWebDriver browser;
+
+        public GoogleSearchTest()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_Markowicz_Go()
+        {
+            //throw new Exception();
+
+            browser.Navigate().GoToUrl("http://markowicz.pro");
+
+            var Articles = browser.FindElement(By.LinkText("O transformacjach"));
+            Articles.Click();
+            Articles.Submit();
+
+            var link = title.FindElements(By.TagName("a"));
+
+            IWebElement expected = null;
+
+            foreach (var expectedtitle in link)
+            {
+                var link = expectedtitle.FindElement(By.CssSelector(""));
+                if (link.GetAttribute("href") == "http://markowicz.pro/author/rafal-markowicz/")
+
+                {
+                    title = expectedtitle;
+                    break;
+                }
+
+            }
+
+            browser.Quit();
+            Assert.NotNull(expected);
+        }
+
+        public void Dispose()
+        {
+            if (browser == null)
+
+            {
+                browser.Quit();
+
+            }
+        }
+    }
+}
+
+
+
