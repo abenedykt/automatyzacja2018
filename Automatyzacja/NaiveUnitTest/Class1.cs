eecee4b1ee2e153cf7ae407bc4eb7cd864fa29e9
Justyna <j_rybarz@o2.pl>
2018-11-27 16:34:24 +0100

    nowy unit test

---
+++ Automatyzacja/NaiveUnitTest/Class1.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GoogleTests
+{
+    public class GoogleSearchTests : IDisposable
+    {
+        private IWebDriver browser;
+
+        public GoogleSearchTests()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void CanGoogleOut_code_sprinters()
+        {
+            browser.Navigate().GoToUrl("http://google.com");
+
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.SendKeys("code sprinters");
+            queryBox.Submit();
+
+            var searchResults = browser.FindElements(By.CssSelector("div.rc"));
+
+            IWebElement expected = null;
+
+            foreach (var result in searchResults)
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                {
+                    expected = result;
+                    break;
+                }
+            }
+
+            Assert.NotNull(expected);
+        }
+
+        public void Dispose()
+        {
+            if (browser != null)
+            {
+                browser.Quit();
+            }
+
+        }
+    }
+}
