721d41eafcc54b5626dbab1a099aca6118724ab7
Admin <milena@example.com>
2018-11-27 18:06:44 +0100

    test markowicz

---
+++ Automatyzacja/NaiveUnitTest/Class2.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NaiveUnitTest
+{
+    public class MarkowiczTests : IDisposable
+    {
+        private IWebDriver broswer;
+
+        public  MarkowiczTests()
+        {
+            broswer = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_markowicz_out_o_transformacjach()
+        {
+            broswer.Navigate().GoToUrl("http://markowicz.pro");
+
+            var linkdonotki = broswer.FindElement(By.XPath("//a[text()='O transformacjach']"));
+            linkdonotki.Click();
+
+            var linkdoautora = broswer.FindElement(By.CssSelector(".byline .author a"));
+            Assert.Equal("Rafał", linkdoautora.Text);
+
+            Assert.Equal("http://markowicz.pro/author/rafal-markowicz/", linkdoautora.GetAttribute("href"));
+           
+
+        }
+
+        public void Dispose()
+        {
+            broswer.Quit();
+        }
+    }
+}
