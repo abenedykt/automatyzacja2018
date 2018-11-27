6771e22dbc7624bf3efd667c290ad573a480f0b3
pzajac <test@mail.pl>
2018-11-27 15:08:07 +0100

    Pierwsza prosta wersja aplikacji

--- Automatyzacja/Automatyzacja/Program.cs
+++ Automatyzacja/Automatyzacja/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace Automatyzacja
 {
@@ -10,6 +12,25 @@ namespace Automatyzacja
     {
         static void Main(string[] args)
         {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://google.com");
+
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.SendKeys("code sprinters");
+            queryBox.Submit();
+
+            var searchResult = browser.FindElements(By.CssSelector("div.rc"));
+            foreach (var result in searchResult)
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://codesprinters.com/")
+                {
+                    Console.WriteLine("Znalazłem");
+                    break;
+                }
+            }
+
+            //browser.Quit();
         }
     }
 }
