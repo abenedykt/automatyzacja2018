605cdd4628b10e260650785b0dbe13381f215910
Krzysztof Baran <krzysztof.baran30@gmail.com>
2018-11-27 15:07:32 +0100

    Zmiany w Kodzie

--- Automatyzacja/Automatyzacja/Program.cs
+++ Automatyzacja/Automatyzacja/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,29 @@ namespace Automatyzacja
     {
         static void Main(string[] args)
         {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://google.com");
+
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.SendKeys("codesprinters");
+            queryBox.Submit();
+
+            var serchResults = browser.FindElements(By.CssSelector("div.rc"));
+
+            foreach (var result in serchResults)
+            {
+              var link =  result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                {
+                    Console.WriteLine("Plik znaleziony");
+                    break;
+                }
+
+            }
+
+
+            //browser.Quit();
+
         }
     }
 }
