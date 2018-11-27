114831f06b24606f30296f56d2b5d6c89a0f1b71
Admin <mieczyslaw88pekacki@gmail.pl>
2018-11-27 16:35:42 +0100

    Pierwszy test Mariusz poprawki

--- Automatyzacja/FirstTestMariusz/Class1.cs
+++ Automatyzacja/FirstTestMariusz/Class1.cs
@@ -10,12 +10,18 @@ using Xunit;
 
 namespace FirstTestMariusz
 {
-    public class Class1
+    public class GoogleSearchTests : IDisposable
     {
-        [Fact]
-        public void ExampleTest()
+        private IWebDriver browser;
+
+        public GoogleSearchTests()
         {
-            IWebDriver browser = new ChromeDriver();
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_google_out_code_sprinters()
+        {
             browser.Navigate().GoToUrl("https://google.com");
 
             var queryBox = browser.FindElement(By.Name("q"));
@@ -24,16 +30,25 @@ namespace FirstTestMariusz
 
             var searchResults = browser.FindElements(By.CssSelector("div.rc"));
 
+            IWebElement expected = null;
             foreach (var result in searchResults)
             {
                 var link = result.FindElement(By.CssSelector(".r>a"));
                 if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
                 {
-                    
+                    expected = result;
                     break;
                 }
             }
-            browser.Quit();
+            Assert.NotNull(expected);
+        }
+        
+        public void Dispose()
+        {
+            if (browser != null)
+            {
+                browser.Quit();
+            }
         }
     }
 }
