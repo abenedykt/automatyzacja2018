5dec5e76007e88b2381c62e893b36ed20797215c
Henry131 <mateuszmitura88@gmail.com>
2018-11-27 15:08:30 +0100

    Pierwszy test

--- automatyzacja/automatyzacja/Program.cs
+++ automatyzacja/automatyzacja/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,23 @@ namespace automatyzacja
     {
         static void Main(string[] args)
         {
+            var browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://google.com");
+            var searchbox = browser.FindElement(By.Name("q"));
+            searchbox.SendKeys("Code sprinters");
+            searchbox.Submit();
+            var results = browser.FindElements(By.CssSelector("div.rc"));
 
+            foreach (var result in results)
+            {
+                result.FindElement(By.CssSelector(".r>a"));
+                if (result.GetAttribute("href") == "http://agileszkolenia.pl/")
+                    break;
+            }
+
+            //publi class var <= do zrobienia (podobno kuku można sobie zrobic)
+
+            browser.Quit();
         }
     }
 }
