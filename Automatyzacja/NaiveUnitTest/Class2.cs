0c89b730094d035275a956d12b4ba1a8665cb961
Justyna <j_rybarz@o2.pl>
2018-11-27 18:26:35 +0100

    new test find rafal

---
+++ Automatyzacja/NaiveUnitTest/Class2.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NewPageMarkowicz
+{
+    public class Markowicz : IDisposable
+    {
+        private IWebDriver browser;
+       
+        public Markowicz()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Markowicz_Find_Rafał()
+        {
+            browser.Navigate().GoToUrl("http://markowicz.pro");
+
+            var row = browser.FindElement(By.XPath("//a[text()='O transformacjach']"));
+
+            Assert.True(row.Text == "O transformacjach");
+            row.Click();
+
+            var name = browser.FindElements(By.CssSelector("span > a"));
+
+            IWebElement expected = null;
+            foreach (var result in name)
+            {                
+                if (result.GetAttribute("href") == "http://markowicz.pro/author/rafal-markowicz/")
+                {
+                    expected = result;
+                    break;
+                }
+           
+            }
+            Assert.NotNull(expected);
+        }
+
+        public void Dispose()
+        {
+            if (browser != null)
+            {
+                browser.Quit();
+            }
+        }
+
+    }
+}
