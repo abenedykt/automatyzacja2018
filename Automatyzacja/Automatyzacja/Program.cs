260dc91ffd2ea51e4f5188a4f9a95ff17ec66a12
Admin <mieczyslaw88pekacki@gmail.pl>
2018-11-27 12:08:31 +0100

    NowaAplikacjaMariusza1

---
+++ Automatyzacja/Automatyzacja/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatyzacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
