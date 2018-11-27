186e3f2ee07dd1924691d90331e8f0afe19d1356
Admin <milena@example.com>
2018-11-27 15:07:15 +0100

    znalezienie elementu na stronie

--- Automatyzacja/Automatyzacja/Program.cs
+++ Automatyzacja/Automatyzacja/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,27 @@ namespace Automatyzacja
     {
         static void Main(string[] args)
         {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://google.com");
+
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.SendKeys("code sprinters");
+            queryBox.Submit();
+
+            var searchResult = browser.FindElements(By.CssSelector("div.rc"));
+
+            foreach (var result in searchResult)
+
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if(link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                {
+                    Console.WriteLine("Znalazlem");
+                    break;
+                }
+            }
+
+            //browser.Quit();
         }
     }
 }
