e29ff90fdd07511c84945fd5b7dee14f786de5e9
Admin <mieczyslaw88pekacki@gmail.pl>
2018-11-27 15:50:33 +0100

    Pierwszy test Mariusz

---
+++ Automatyzacja/FirstTestMariusz/Class1.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace FirstTestMariusz
+{
+    public class Class1
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://google.com");
+
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.SendKeys("code sprinters");
+            queryBox.Submit();
+
+            var searchResults = browser.FindElements(By.CssSelector("div.rc"));
+
+            foreach (var result in searchResults)
+            {
+                var link = result.FindElement(By.CssSelector(".r>a"));
+                if (link.GetAttribute("href") == "http://agileszkolenia.pl/")
+                {
+                    
+                    break;
+                }
+            }
+            browser.Quit();
+        }
+    }
+}
