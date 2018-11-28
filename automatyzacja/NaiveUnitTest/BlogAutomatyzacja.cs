71288b09fc192a615b855d04169e3d900a45c068
Ela <ekuch@gmail.com>
2018-11-28 11:19:00 +0100

    potwierdz komentarz

--- automatyzacja/NaiveUnitTest/BlogAutomatyzacja.cs
+++ automatyzacja/NaiveUnitTest/BlogAutomatyzacja.cs
@@ -18,15 +18,29 @@ namespace NaiveUnitTest
             browser = new ChromeDriver();
         }
 
+      private string GenerateEmail()
+            {
+                var user = Guid.NewGuid().ToString();
+                return $"{user}@nonexistent.test.com";
+
+            }
+
         [Fact]
         public void Wpisz_komentarz()
         {
+           
+
             browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net");
             var komentarz = browser.FindElement(By.CssSelector("span.comments-link"));
             komentarz.Click();
             var wkoment = browser.FindElement(By.Name("comment"));
-                
-            wkoment.SendKeys("Moj komentarz, Ela");
+
+            var ktext = Guid.NewGuid().ToString();
+
+
+            wkoment.SendKeys(ktext);
+
+
             wkoment.Click();
 
             //wkoment.Submit();
@@ -34,10 +48,16 @@ namespace NaiveUnitTest
             var authorkom = browser.FindElement(By.Name("author"));
             authorkom.SendKeys("ElaK");
             var mailkom = browser.FindElement(By.Name("email"));
-            mailkom.SendKeys("elakuch@gmail.com");
+            mailkom.SendKeys(GenerateEmail());
             var opublkom = browser.FindElement(By.Name("submit"));
             opublkom.Click();
 
+
+            var comments = browser.FindElements(By.CssSelector(".comment-content"));
+
+            Assert.NotNull(comments.Single(x=>x.Text == ktext));
+
+            //Assert.Equal("Rafał", autor.Text);
 
 
 
