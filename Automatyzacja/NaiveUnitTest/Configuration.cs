6083b08fe3be993b72405a63c045af2f02e6f4bb
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-11-28 15:12:38 +0100

    added skeleton for login and logout scenario

--- Automatyzacja/NaiveUnitTest/Configuration.cs
+++ Automatyzacja/NaiveUnitTest/Configuration.cs
@@ -3,5 +3,7 @@
     internal class Configuration
     {
         public const string BlogUrl = "http://automatyzacja.benedykt.net";
+
+        public const string BlogAdminUrl = "http://automatyzacja.benedykt.net/wp-admin";
     }
 }
