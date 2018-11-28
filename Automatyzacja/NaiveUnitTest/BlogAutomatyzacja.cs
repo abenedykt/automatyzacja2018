74192662f45c91c01f3be02b943e130b7be8ffed
Admin <milena@example.com>
2018-11-28 11:23:39 +0100

    sprawdzenie czy dodany komenatrz

--- Automatyzacja/NaiveUnitTest/BlogAutomatyzacja.cs
+++ Automatyzacja/NaiveUnitTest/BlogAutomatyzacja.cs
@@ -22,29 +22,55 @@ namespace NaiveUnitTest
 
         public void add_comment()
         {
+            var autor = "Milena-" + Guid.NewGuid().ToString();
+
             broswer.Navigate().GoToUrl("http://automatyzacja.benedykt.net");
 
             var linkKomenatarz = broswer.FindElement(By.CssSelector("span.comments-link"));
             linkKomenatarz.Click();
 
             var oknoKomentarz = broswer.FindElement(By.Name("comment"));
-            oknoKomentarz.SendKeys("komentarz Mileny");
+            oknoKomentarz.SendKeys(Guid.NewGuid().ToString());
 
             var podpis = broswer.FindElement(By.Name("author"));
-            podpis.SendKeys("Milena");
+            podpis.SendKeys(autor);
 
             var email = broswer.FindElement(By.Name("email"));
-            email.SendKeys("email@email.com");
+            email.SendKeys(GenerateEmail());
 
             var opublikujKomentarz = broswer.FindElement(By.Id("submit"));
             opublikujKomentarz.Click();
+
+            IWebElement MojKomentarz = null;
+
+            var Komentarze = broswer.FindElements(By.ClassName("comment-body"));
+            foreach (var Komentarz in Komentarze)
+            {
+                if (Komentarz.FindElement(By.CssSelector(".comment-author b")).Text == autor)
+                {
+                    MojKomentarz = Komentarz;
+                    break;
+
+                }
+            }
+
+            Assert.NotNull(MojKomentarz);
+
+        }
+
+
+
+        private string GenerateEmail()
+        {
+            var user = Guid.NewGuid().ToString();
+            return $"{user}@email.com";
 
         }
 
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            broswer.Quit();
         }
     }
 }
