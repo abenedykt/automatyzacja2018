2ae5a93268288f7cbee5be7d98f4928326856370
Gabriela Krystek <gabriela.krystek91@gmail.com>
2018-11-29 10:06:46 +0100

    WordpressTests

---
+++ Automatyzacja/WordpressPageObjectTests/Wordpress_tests.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace WordpressPageObjectTests
+{
+    public class Wordpress_tests : IDisposable
+    {
+        private IWebDriver _browser;
+
+        public Wordpress_tests()
+        {
+            _browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_add_and_publish_new_note()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            try //po to, żeby posprzątać :)
+            {
+                _browser.Quit();
+                _browser.Dispose();
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+    }
+}
