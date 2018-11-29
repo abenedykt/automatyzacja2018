3899e73ab1f94fb91ad3824917065a7ec98038bb
Admin <mariola.zdulska.mz@gmail.com>
2018-11-29 10:05:59 +0100

    zapisuje

---
+++ Automatyzacja/WordpressPage/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WordpressPageObjectTests
+
+{
+    public class Wordpress_tests
+    {
+        [Fact]
+        public void Can_and_and_publish_new_note()
+        {
+
+        }
+    }
+}
