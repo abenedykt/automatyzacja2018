eb3e3f5f7b377f24ef59c42f6a5d00cfa1601101
Krzysztof Baran <krzysztof.baran30@gmail.com>
2018-11-29 10:06:07 +0100

    Wordpress Object Page

---
+++ Automatyzacja/WordPressPageObject/Word_Press_Test_Page_Object.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace WordPressPageObject
+{
+    public class Wrod_Press_Test_Page_Object : IDisposable
+    {
+        private IWebDriver _browser;
+
+        public Wrod_Press_Test_Page_Object()
+        {
+            _browser = new ChromeDriver();
+        }
+
+        [Fact]
+
+        public void Cann_Add_and_publish_New_nowe()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _browser.Quit();
+                _browser.Dispose();
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
+
+
+
+
