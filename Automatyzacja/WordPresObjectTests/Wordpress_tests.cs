120e829fc4eba40f8ab4c1f0c2fbdb193d0c57a9
Admin <mieczyslaw88pekacki@gmail.pl>
2018-11-29 10:06:16 +0100

    Oststni dzień test

---
+++ Automatyzacja/WordPresObjectTests/Wordpress_tests.cs
@@ -0,0 +1,35 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace WordPresObjectTests
+{
+    public class Wordpress_tests : IDisposable
+    {
+        private IWebDriver _browser;
+
+        public Wordpress_tests()
+        {
+            _browser = new ChromeDriver();
+        }
+        [Fact]
+        public void Can_add_and_publish_new_note()
+        {
+
+        }
+            public void Dispose()
+        {
+            try
+            {
+                _browser.Quit();
+                _browser.Dispose();
+            }  
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
